f5561ee30a555e157368af7cdb14e7d591e68e95
Floris van Breugel <florisvb@gmail.com>
2012-08-12 20:08:25 -0700

    new setup.py for new filestructure

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.0.1',
     author='Floris van Breugel',
     author_email='floris@caltech.edu',
-    packages = ['ransac_fit', 'fit_data'],
+    packages = ['data_fit'],
     license='BSD',
     description='line and curve fitting',
     long_description=open('README.txt').read(),
